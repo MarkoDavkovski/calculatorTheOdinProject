939a50a1fd040d0a2b1bc2b43ca778b250e6048a
Marko Davkovski <marko.davkovski@gmail.com>
2022-11-22 00:13:04 +0100

    Basic functionality

--- main.js
+++ main.js
@@ -1,40 +1,91 @@
-const btn = document.querySelectorAll('.btn');
-const currentOperation = document.querySelector('.currentOperation');
-const previousOperation = document.querySelector('.previousOperation');
+let currNum = '';
+let prevNum = '';
+let operator = '';
+let res = '';
 
+const numBtn = document.querySelectorAll('[data-number]');
+const operationBtn = document.querySelectorAll('[data-operation]');
+const clearBtn = document.querySelector('[clearBtn]');
+const equalsBtn = document.querySelector('[data-equals]');
+const currOutput = document.querySelector('.currOutput');
+const prevOutput = document.querySelector('.prevOutput');
+
+numBtn.forEach((btn) => {
+	btn.addEventListener('click', (e) => {
+		handleNumber(e.target.textContent);
+	});
+});
+
+function handleNumber(num) {
+	currNum += num;
+	currOutput.textContent = currNum;
+}
+
+operationBtn.forEach((op) => {
+	op.addEventListener('click', (e) => {
+		handleOperator(e.target.textContent);
+	});
+});
+
+function handleOperator(op) {
+	currNum = currOutput.textContent;
+	currNum = parseInt(currNum);
+	operator = op;
+	prevNum = currNum;
+	currNum = '';
+	prevOutput.textContent = prevNum + ' ' + op;
+	currOutput.textContent = '';
+}
+
+function equals() {
+	prevOutput.textContent = prevOutput.textContent + ' ' + currNum;
+	// currNum = parseInt(currNum);
+	// prevNum = parseInt(prevNum);
+	currOutput.textContent = operate(operator, prevNum, currNum);
+}
+
+function del() {}
+function clearFun() {
+	currNum = '';
+	prevNum = '';
+	operator = '';
+	currOutput.textContent = '0';
+	prevOutput.textContent = '';
+}
 function add(a, b) {
-	return a + b;
+	res = parseInt(a) + parseInt(b);
+	return res;
 }
 
 function subtract(a, b) {
-	return a - b;
+	res = a - b;
+	return res;
 }
 
 function multiply(a, b) {
-	return a * b;
+	res = a * b;
+	return res;
 }
 
 function divide(a, b) {
-	return a / b;
+	res = a / b;
+	return res;
 }
 
 function operate(op, a, b) {
 	switch (op) {
 		case '+':
-			add(a, b);
-			break;
+			return add(a, b);
+
 		case '-':
-			subtract(a, b);
-			break;
-		case '*':
-			multiply(a, b);
-			break;
-		case '/':
-			divide(a, b);
-			break;
-		case 'pow':
-			pow(a, b);
-			break;
+			return subtract(a, b);
+
+		case 'x':
+			return multiply(a, b);
+
+		case '÷':
+			return divide(a, b);
+
 		default:
 			'Enter a valid operator';
 	}
