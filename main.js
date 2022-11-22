6ef27061ba48fe174bbe4849a61222f1d4753404
Marko Davkovski <marko.davkovski@gmail.com>
2022-11-22 22:44:06 +0100

    Minor bug fixes

--- main.js
+++ main.js
@@ -11,6 +11,8 @@ const currOutput = document.querySelector('.currOutput');
 const prevOutput = document.querySelector('.prevOutput');
 const decimal = document.querySelector('[data-decimal]');
 
+window.addEventListener('keydown', handleKeyboard);
+
 numBtn.forEach((btn) => {
 	btn.addEventListener('click', (e) => {
 		handleNumber(e.target.textContent);
@@ -24,7 +26,7 @@ function addDecimal() {
 }
 
 function handleNumber(num) {
-	if (currNum.length > 15) return;
+	if (currNum.length > 10) return;
 	currNum += num;
 	currOutput.textContent = currNum;
 }
@@ -40,11 +42,43 @@ function handleOperator(op) {
 	operator = op;
 	prevNum = currNum;
 	currNum = '';
+
 	prevOutput.textContent = prevNum + ' ' + op;
 	currOutput.textContent = '';
 }
 
+function handleKeyboard(e) {
+	if (e.key >= 0 && e.key <= 9) {
+		handleNumber(e.key);
+	} else if (e.key === '.') addDecimal();
+	else if (e.key === '=') equals();
+	else if (e.key === '+' || e.key === '-' || e.key === '*' || e.key === '/')
+		convertOperator(e.key);
+	else if (e.key === 'Backspace') del();
+	else if (e.key === 'Escape') clearFun();
+}
+function convertOperator(op) {
+	if (op === '+') return handleOperator('+');
+	if (op === '-') return handleOperator('-');
+	if (op === '*') return handleOperator('x');
+	if (op === '/') return handleOperator('÷');
+}
+
 function equals() {
+	// let lastChar = prevOutput.textContent.toString().slice(-1);
+	// if (
+	// 	lastChar === '+' ||
+	// 	lastChar === '-' ||
+	// 	lastChar === 'x' ||
+	// 	lastChar === '÷'
+	// ) {
+	// 	prevOutput.textContent = prevOutput.textContent
+	// 		.toString()
+	// 		.slice(0, -1)
+	// 		.concat(' ' + operator);
+	// }
+	if (operator === '÷' && currNum === '0')
+		return alert('You cannot divide by zero');
 	prevOutput.textContent = prevOutput.textContent + ' ' + currNum;
 	currOutput.textContent = operate(operator, prevNum, currNum);
 	currNum = currOutput.textContent;
